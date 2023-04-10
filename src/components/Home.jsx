c2bbff0614993667c7b3ad05c7626234bf2ec3e9
Franco Vidales <francovidales@MacBook-Air-de-Franco.local>
2023-04-10 18:31:05 -0300

    Update0.4

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import "../styles/home.scss"
+
+
 export const Home = () => {
     return (
         <>
             <div className='box-home'>
-                <h1>Welcome to my portfolio</h1>
-                <h2>I am Franco Vidales and i'm Front End Develop</h2>
+                <div className='text-home'>
+                    <div className='welcome-text-home'>
+                        <h3>Hi, I'am Franco Vidales, and I'am</h3>
+                    </div>
+                    <div className='title-home'>
+                        <h1>Front-End Developer</h1>
+                    </div>
+                </div>
+                <div className='img-home'>
+                    <img src="" alt="" />
+                </div>
             </div>
         </>
     )
