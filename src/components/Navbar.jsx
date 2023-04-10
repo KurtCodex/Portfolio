d1bc76db325c09a5414d0d550a40a839100948ed
Franco Vidales <francovidales@MacBook-Air-de-Franco.local>
2023-04-09 21:02:49 -0300

    Update0.3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,6 +30,7 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className={`Background-blackblur ${clicked ? 'active' : ''}`}>
+
             </div>
         </>
     )
